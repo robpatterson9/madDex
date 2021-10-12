7dc6a9a09f9e676a67036d85557c768b8fd72667
robert patterson <rob@robpatterson.net>
2021-10-11 19:16:51 -0700

    more

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -11,7 +11,6 @@ import { swapSectionData, earnSectionData, cakeSectionData } from './components/
 
 import SalesSection from './components/SalesSection'
 
-
 const StyledHeroSection = styled(PageSection)`
   padding-top: 116px;
 
@@ -36,19 +35,20 @@ const UserBannerWrapper = styled(Container)`
   }
 `
 
-
-
-
 const Home: React.FC = () => {
   const { theme } = useTheme()
   const { account } = useWeb3React()
 
-  const HomeSectionContainerStyles = { background: '#ffffff', margin: '0', width: '50%', maxWidth: '968px', marginBottom: '1300px' }
+  const HomeSectionContainerStyles = {
+    background: '#ffffff',
+    margin: '0',
+    width: '50%',
+    maxWidth: '968px',
+    marginBottom: '1300px',
+  }
 
   return (
     <>
-
-
       {/* <PageSection
         innerProps={{ style: { margin: '0', width: '100%' } }}
         background={
@@ -66,39 +66,28 @@ const Home: React.FC = () => {
         // background={theme.colors.background}
         index={2}
         hasCurvedDivider={false}
-        background="./MAD_main.svg"  >
-
-
-        <SalesSection
-
-
-
-          {...swapSectionData} />
+        background="./MAD_main.svg"
+      >
+        <SalesSection {...swapSectionData} />
         <div>
-
-          <br /><br />
+          <br />
+          <br />
           <br />
 
           <iframe
             src="https://widget.onramper.com??defaultCrypto=BNB&color=FF1100&apiKey=pk_prod_EMEhKOXNuUfrHnqu9Xg2w3hUVBPuurn8sHoPYvbbf4s0"
             height="595px"
             width="540px"
-
             title="Onramper widget"
             frameBorder="0"
-            allow="accelerometer;
- autoplay; camera; gyroscope; payment"
-
+            allow="accelerometer; autoplay; camera; gyroscope; payment"
           >
-            <a href="https://widget.onramper.com" target="_blank" rel="noreferrer">Buy crypto</a>
+            <a href="https://widget.onramper.com" target="_blank" rel="noreferrer">
+              Buy crypto
+            </a>
           </iframe>
-
         </div>
-
-
       </PageSection>
-
-
     </>
   )
 }
